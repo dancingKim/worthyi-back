c575e9f255e206523e6b1897aa1c68ff70a195a4
rightlightfg <rightlightfg@gmail.com>
2024-09-14 16:09:04 +0900

    ADD : set project directory

---
+++ src/main/java/com/worthyi/worthyi_backend/WorthyIApplication.java
@@ -0,0 +1,13 @@
+package com.worthyi.worthyi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorthyIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorthyIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worthyi/worthyi_backend/WorthyIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worthyi.worthyi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorthyIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
